6cbcc61ad6aa6146057c25972efa96d2c8fbfa7c
Jeferson Farias Calazans <calazans10@gmail.com>
2017-02-01 18:42:23 -0200

    Update solution 028

--- problem 028/code.rb
+++ problem 028/code.rb
@@ -1,4 +1,10 @@
 def solution(n)
+  result = 1
+  (3..n).step(2).each { |i| result += 4 * i**2 - 6 * i + 6 }
+  result
+end
+
+def fast_solution(n)
   (4 * n**3 + 3 * n**2 + 8 * n - 9) / 6
 end
 
@@ -6,14 +12,14 @@ require 'minitest/autorun'
 
 class Tests < Minitest::Test
   def test_example_input1
-    assert_equal 25, solution(3)
+    assert_equal 25, fast_solution(3)
   end
 
   def test_example_input2
-    assert_equal 101, solution(5)
+    assert_equal 101, fast_solution(5)
   end
 
   def test_example_input3
-    assert_equal 669_171_001, solution(1001)
+    assert_equal 669_171_001, fast_solution(1001)
   end
 end
